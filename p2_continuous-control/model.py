daa600a60f63d3c1d2209ba4951a47de13263986
francelico <samuel.garcin@wecorpindustries.com>
2019-09-13 00:07:03 +0100

    feat: coded training method. however the agent is not learning past a score of ~0.8. not sure why

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -27,6 +27,10 @@ class Actor(nn.Module):
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
+        
+        #Normalisation layer
+        self.bn1 = nn.BatchNorm1d(fc1_units)
+        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -36,7 +40,13 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
+        
+        # Reshape the state to comply with Batch Normalization
+        if state.dim() == 1:
+            state = torch.unsqueeze(state,0)
+        
         x = F.relu(self.fc1(state))
+        x = self.bn1(x)
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
 
@@ -59,6 +69,10 @@ class Critic(nn.Module):
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, 1)
+        
+        #Normalisation layer
+        self.bn1 = nn.BatchNorm1d(fcs1_units)
+        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -69,6 +83,7 @@ class Critic(nn.Module):
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         xs = F.relu(self.fcs1(state))
+        xs = self.bn1(xs)
         x = torch.cat((xs, action), dim=1)
         x = F.relu(self.fc2(x))
         return self.fc3(x)
